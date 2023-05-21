1f5edb9b1a12d507a9e0ffb2569ffe99178ccf19
Mati-IR <mpabich4@gmail.com>
2023-05-21 13:08:20 +0200

    New files

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
+
 import cv2
+import argparse
 
-# Load image, grayscale, Gaussian blur, Otsu's threshold
-image = cv2.imread("test.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (1,1), 0)
-thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+# --image : Source image
+# --mask : Binary mask indicating pixels to be inpainted
+# --method : Inpainting algorithm to be used Telea/NS
+# --radius : Radius of neighborhood pixels to be considered for interpolation purpose
 
-# OPTIONAL to filter out small/large dots using contour area filtering
-# Adjust the area to only keep larger dots
-'''
-DOT_AREA = 10
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-for c in cnts:
-    area = cv2.contourArea(c)
-    if area < DOT_AREA:
-        cv2.drawContours(thresh, [c], -1, 0, -1)
-'''
+a = argparse.ArgumentParser()
+a.add_argument("-i", "--image", type=str, required=False, default="a.jpg", help="path of image to be used")
+a.add_argument("-m", "--mask", type=str, required=False, default="maska.png", help="path of masked image")
+a.add_argument("-a", "--method", type=str, default="telea", choices=["telea", "ns"], help="inpainting algorithm")
+a.add_argument("-r", "--radius", type=int, default=3, help="inpainting radius")
+args = vars(a.parse_args())
 
-# Bitwise_and to extract dots
-result = cv2.bitwise_and(image, image, mask=thresh)
-result[thresh==0] = 255
+paint_method = cv2.INPAINT_TELEA
 
-cv2.imshow("gray", gray)
-cv2.imshow("thresh", thresh)
-cv2.imshow("result", result)
-cv2.waitKey()
+if args["method"] == "ns":
+    paint_method = cv2.INPAINT_NS
+
+image = cv2.imread(args["image"])
+mask = cv2.imread(args["mask"])
+# convert to gray scale - one channel image
+mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+# calling the inpaint function 
+output = cv2.inpaint(image, mask, args["radius"], flags=paint_method)
+
+cv2.imshow('Image', image)
+cv2.imshow('Mask', mask)
+cv2.imshow('Output', output)
+cv2.imwrite('output.jpg', output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#view rawimage_inpainting.py# hosted with ❤ by GitHub
